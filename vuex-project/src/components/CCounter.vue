f5b57a35f2c0cf76ebc987a7f173342c3fe0a49f
Anna Fernandes <anna.cbf@gmail.com>
2021-04-07 16:54:20 -0300

    Added vuex and client folders

---
+++ vuex-project/src/components/CCounter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+        <h3>Composition API + Vuex</h3>
+        <h5>{{ counter }} x 2 = {{ times2 }}  </h5>
+        <button @click="inc">Increment</button>
+    </div>
+</template>
+
+<script>
+import { useState, useGetters, useMutations } from '@/helpers.ts'
+
+export default {
+    setup() {
+        const { counter } = useState(['counter'])
+        const { times2 } = useGetters(['times2'])
+        const { setCounter } = useMutations(['setCounter'])
+        const inc = () => setCounter(counter.value + 1)
+        return { inc, counter, times2 }
+    },
+}
+</script>
---
+++ vuex-project/src/views/OCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <OCounter />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import OCounter from '@/components/OCounter.vue'; // @ is an alias to /src
+
+@Options({
+  components: {
+    OCounter,
+  },
+})
+export default class Option extends Vue {}
+</script>
